dcb838438191173aabda1354765676e86ae95731
myoungji-kim <myoung9412@daum.net>
2022-03-07 11:01:42 +0900

    깃 코드 추가 수정 후 커밋

--- src/GitTestMain.java
+++ src/GitTestMain.java
@@ -4,6 +4,7 @@ public class GitTestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git-Test");
+		System.out.println("Master 브랜치");
 	}
 
 }
