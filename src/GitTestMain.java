163002adbe67522d07db69e56ded91f45316d9c8
myoungji-kim <myoung9412@daum.net>
2022-03-07 11:41:17 +0900

    텍스트출려어어억

--- src/GitTestMain.java
+++ src/GitTestMain.java
@@ -4,8 +4,6 @@ public class GitTestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git-Test");
-		System.out.println("Master 브랜치");
-		System.out.println("Master 브랜치2");
 	}
 
 }
