5f337fc1f12c493cc77d35d7e9679f27a3b1297f
myoungji-kim <myoung9412@daum.net>
2022-03-07 12:14:39 +0900

    Hotfix Branch 추가 테스트

--- src/GitTestMain.java
+++ src/GitTestMain.java
@@ -4,6 +4,9 @@ public class GitTestMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git-Test");
+		System.out.println("Master Branch");
+		
+		System.out.println("Hotfix Branch");
 	}
 
 }
